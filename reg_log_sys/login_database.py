137b992e2bb22ba4b89fd85bcef1fff5541723f7
Arbaaz Zakir <arbaazzakir@Arbaazs-MacBook-Pro.local>
2020-06-06 22:43:38 +0100

    user data can now not be regestered to the database if all fields not inputted

--- reg_log_sys/login_database.py
+++ reg_log_sys/login_database.py
@@ -11,12 +11,18 @@ db = client.blockchain
 #           "karma": 0}
 #db.user.insert_one(post_ex)
 
+
+#music_post = {"title": 0,
+#           "artist": "Arbaaz",
+#           "writer": "Zakir",
+#           "username": "az16aan",
+#           "others": 0}
+
+
 #print("welcome to blockchain music temporary data storage system, what would you like to do today?")
 #print(db.users.find({"_id": 0}))
-global session
+session = ""
 global id_num
-#while state != 'exit':
- #   tmp.show_cmds()
 
 
 class Temp:
@@ -34,6 +40,7 @@ class Temp:
         
         if login_user:
             if login_user["password"] == passwd:
+                global session 
                 session = username
                 print ('welcome back: ' + session)
                 
@@ -52,7 +59,8 @@ class Temp:
         existing_user = users.find_one({'username': username})
         if existing_user:
             print('username taken')
-        
+        elif fname is "" or lname is "" or username is "" or password is "":
+            print("failed to input 1 or more appropriate fields!")
         else: 
             db.user.insert_one({ "firstname":fname,
                                  "lastname":lname,
@@ -61,9 +69,49 @@ class Temp:
                                  "Reputation": reputation, 
                                  "karma":karma})
     
-            print('regestration sucessful')
+            print('registration successful')
             
-
+        
+    def add_song(self):
+        global session
+        music = db.music
+        music_title = input("input new title:\n")
+        new_lyrics = input("input new lyrics:\n")
+        artist = input("input new writer:\n")
+        writer = input("input new writer:\n")
+        
+        index = -1
+        other_state = "add new"
+        others = ["","","","","","",""]
+        while other_state is not "done":
+            other_inputs = input("input new others to credit:\n") # loop array
+            if other_inputs == "done":
+                other_state = "done"
+                
+            index+=1
+            others[index] ==[other_inputs]
+        infringement = []
+        it_lyrics = music.find({"lyrics"})
+        if music.count() != 0:
+            for lyrics in it_lyrics:
+                to_compare = lyrics["lyrics"]
+                similarity_percent = lyrics_similarity(new_lyrics, to_compare)
+            
+                if similarity_percent > 0.50:
+                    infringement.append([lyrics["username"], lyrics["title"], similarity_percent])
+                
+        db.music.insert_one({ "lyrics": new_lyrics,
+                              "title": music_title,
+                              "artist": artist,
+                              "writer": writer,
+                              "username": session,
+                              "others": others,
+                              "infringement": infringement})
+        
+        
+    def lyrics_similarity(song1, song2):
+        return SequenceMatcher(None, song1, song2).ratio()
+        
 
 #if __name__ == '__main__':     
 tmp = Temp()
@@ -87,7 +135,8 @@ while state is not 'exit':
         print("thank you, goodbye")
         state = 'exit'
         
-        
+    elif userin == 'A' or userin =='a':
+        tmp.add_song()
         
         
         
